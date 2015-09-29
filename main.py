3ff34c5920e9a88a4bc5d0a5b06bc6018c87700e
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-29 15:00:09 +0200

    Found optimal solution...?

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import random
 
 
 def main():
-    machine = SupportVectorMachine()
-    x = (1, 2, 1)
-    y = (2, 3, -1)
-    print(machine.linear_kernel(x, y))
-    print(machine.polynomial_kernel(x, y, 2))
-    print(SupportVectorMachine.dim2(x))
+    data = setup_data()
+    machine = SupportVectorMachine(data)
+
+
+
+def setup_data():
     classA = [(random.normalvariate(-1.5, 1), random.normalvariate(0.5, 1), 1.0) for _ in range(5)] + \
              [(random.normalvariate(1.5, 1), random.normalvariate(0.5, 1), 1.0) for _ in range(5)]
     classB = [(random.normalvariate(0.0, 0.5), random.normalvariate(-0.5, 0.5), -1.0) for _ in range(10)]
     data = classA + classB
     random.shuffle(data)
-    p_matrix = machine.build_p_matrix(data)
-    print(p_matrix)
+    return data
 
 
 if __name__ == '__main__':
--- support_vector_machine.py
+++ support_vector_machine.py
@@ -4,32 +4,50 @@ import numpy, pylab, random, math
 
 
 class SupportVectorMachine:
-    def __init__(self):
-        self.alphas = []
+    def __init__(self, data_points):
+        self.data_points = data_points
+        self.p = self.build_p_matrix(data_points)
+        self.q = self.build_q_vector(len(data_points))
+        self.h = self.build_h_vector(len(data_points))
+        self.g = self.build_g_matrix(len(data_points))
+        self.alphas = self.call_qp()
 
-    @staticmethod
-    def linear_kernel(x, y):
+    def linear_kernel(self, x, y):
         return numpy.transpose(x).dot(y) + 1
 
-    @staticmethod
-    def polynomial_kernel(x, y, p):
+    def polynomial_kernel(self, x, y, p):
         return math.pow(numpy.transpose(x).dot(y) + 1, p)
 
-    @staticmethod
     def radial_basis_kernel(self):
         pass
-
-    @staticmethod
-    def dim2(data_point):
-        return data_point[0:2]
 
     def sigmoid_kernel(self):
         pass
 
-    @staticmethod
-    def build_p_matrix(data_points):
+    def build_q_vector(self, n):
+        q = numpy.empty(n)
+        q.fill(-1)
+        return q
+
+    def build_h_vector(self, n):
+        return numpy.zeros(n)
+
+    def build_g_matrix(self, n):
+        g_matrix = numpy.zeros([n, n])
+        for i in range(n):
+            g_matrix[i][i] = -1
+        return g_matrix
+
+    def build_p_matrix(self, data_points):
         p_matrix = numpy.empty([len(data_points), len(data_points)])
         for i, x_i in enumerate(data_points):
             for j, x_j in enumerate(data_points):
-                p_matrix[i][j] = x_i[2] * x_j[2] * SupportVectorMachine.linear_kernel(x_i[0:2], x_j[0:2])
+                p_matrix[i][j] = x_i[2] * x_j[2] * self.linear_kernel(x_i[0:2], x_j[0:2])
         return p_matrix
+
+    def call_qp(self):
+        r = qp(matrix(self.p), matrix(self.q), matrix(self.g), matrix(self.h))
+        return list(r['x'])
+
+    def without_zeroes(self, alphas):
+        return [a for a in alphas if abs(a) < 1e-5]
